a7ff112a38c40b7d4a383bf0f3a87cc1a9277610
drd-2rcl <drd.rclrd@gmail.com>
2020-04-29 14:39:12 -0300

    Add initial test

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,3 +1,9 @@
-describe 'description' do
-  
+require 'calculator'
+
+describe Calculator do
+  it 'sum method for 2 numbers' do
+    calc = Calculator.new
+    result = calc.sum(5,5)
+    expect(result).to eq(10)
+  end
 end
