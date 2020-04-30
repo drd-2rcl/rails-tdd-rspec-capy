98a1ce681462fdb9117375817184010cc004f925
drd-2rcl <drd.rclrd@gmail.com>
2020-04-30 11:42:44 -0300

    Add spec to example of matchers boolean

---
+++ exemplo_rspec_tdd/spec/matchers/comparaçao/comparacao_spec.rb
@@ -0,0 +1,31 @@
+describe 'Matchers de Comparação' do
+  it '#equal - Testa se é o mesmo objeto' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).not_to equal(y)
+    expect(x).to equal(x)
+  end
+
+  it '#be - Testa se é o mesmo objeto tbm' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).not_to be(y)
+    expect(x).to be(x)
+  end
+
+  it '#eql - Testa o valor/conteudo' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(x).to eql(x)
+  end
+
+  it '#eq - Testa o valor/conteudo tbm, dizem que "eq" é um alias de "eql"' do
+    x = 'ruby'
+    y = 'ruby'
+
+    expect(y).to eq(y)
+  end
+end
