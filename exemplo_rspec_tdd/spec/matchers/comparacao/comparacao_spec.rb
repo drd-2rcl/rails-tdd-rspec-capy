9f50dbef81f8039fb2435fde73def407a06c30e7
drd-2rcl <drd.rclrd@gmail.com>
2020-05-03 18:13:51 -0300

    be_between inclusive and exclusive

--- exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -16,4 +16,16 @@ describe 'Matchers de Comparação' do
     expect(5). to be <= 12
     expect(5). to be <= 5
   end
+
+  it 'be_between inclusive' do
+    expect(5). to be_between(2, 7).inclusive
+    expect(2). to be_between(2, 7).inclusive
+    expect(7). to be_between(2, 7).inclusive
+  end
+
+  it 'be_between exclusive' do
+    expect(5). to be_between(2, 7).exclusive
+    expect(3). to be_between(2, 7).exclusive
+    expect(6). to be_between(2, 7).exclusive
+  end
 end
